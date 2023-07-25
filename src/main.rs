d148f3ffa6a03770204624e1194da624b4d1287a
Rafael Lemos <contact@flemosr.com>
2023-07-25 20:31:16 -0300

    remove protos not demanded by `tonic_build` on the compilation test

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,20 @@ use glob::glob;
 use std::fs;
 use std::path::PathBuf;
 
-fn collect_protos(repo: &str, glob_pattern: &str) {
+fn collect_protos(repo: &str, glob_patterns: Vec<&str>) {
     let out_dir = PathBuf::from("proto");
 
     let repo = PathBuf::from("submodules").join(repo);
-    let source_protos: Vec<PathBuf> = glob(repo.join(glob_pattern).to_str().unwrap())
-        .unwrap()
-        .filter_map(Result::ok)
-        .collect();
+    let mut source_protos: Vec<PathBuf> = Vec::new();
+
+    for pattern in glob_patterns {
+        let mut matches: Vec<PathBuf> = glob(repo.join(pattern).to_str().unwrap())
+            .unwrap()
+            .filter_map(Result::ok)
+            .collect();
+
+        source_protos.append(&mut matches);
+    }
 
     for proto in source_protos {
         println!("Found proto {:?}", proto);
@@ -31,21 +37,62 @@ fn collect_protos(repo: &str, glob_pattern: &str) {
 }
 
 fn main() {
-    collect_protos("data-plane-api", "envoy/**/v3/*.proto");
-    collect_protos("data-plane-api", "envoy/annotations/*.proto");
-    collect_protos("xds", "udpa/annotations/*.proto");
-    collect_protos("xds", "xds/core/v3/**/*.proto");
-    collect_protos("xds", "xds/annotations/v3/**/*.proto");
-    collect_protos("xds", "xds/type/matcher/v3/**/*.proto");
-    collect_protos("protoc-gen-validate", "**/validate.proto");
-    collect_protos("googleapis", "google/api/expr/v1alpha1/*.proto");
-    collect_protos("opencensus-proto", "**/opencensus/proto/trace/v1/*.proto");
-    collect_protos("opentelemetry-proto", "**/common/v1/*.proto");
-    collect_protos("googleapis", "google/rpc/*.proto");
-    collect_protos("googleapis", "google/api/*.proto");
-    collect_protos("client_model", "io/prometheus/client/*.proto");
+    // let out_dir = PathBuf::from("proto");
+    // if !out_dir.is_dir() {
+    //     panic!("out_dir {:?} not found", out_dir);
+    // }
+    // fs::remove_dir_all(&out_dir).expect("can remove proto dir");
+    // fs::create_dir(&out_dir).expect("can create proto dir");
+
+    collect_protos(
+        "data-plane-api",
+        vec![
+            "envoy/**/v3/*.proto",
+            "envoy/annotations/deprecation.proto",
+            "envoy/annotations/resource.proto",
+        ],
+    );
+    collect_protos(
+        "xds",
+        vec![
+            "udpa/annotations/status.proto",
+            "udpa/annotations/versioning.proto",
+            "udpa/annotations/migrate.proto",
+            "udpa/annotations/sensitive.proto",
+            "udpa/annotations/security.proto",
+            "xds/core/v3/context_params.proto",
+            "xds/core/v3/extension.proto",
+            "xds/core/v3/authority.proto",
+            "xds/core/v3/collection_entry.proto",
+            "xds/core/v3/resource_locator.proto",
+            "xds/annotations/v3/status.proto",
+            "xds/type/matcher/v3/matcher.proto",
+            "xds/type/matcher/v3/string.proto",
+            "xds/type/matcher/v3/regex.proto",
+        ],
+    );
+    collect_protos("protoc-gen-validate", vec!["**/validate.proto"]);
+    collect_protos(
+        "googleapis",
+        vec![
+            "google/api/expr/v1alpha1/checked.proto",
+            "google/api/expr/v1alpha1/syntax.proto",
+            "google/rpc/status.proto",
+            "google/api/annotations.proto",
+            "google/api/http.proto",
+        ],
+    );
     collect_protos(
         "opencensus-proto",
-        "**/opencensus/proto/resource/v1/*.proto",
+        vec![
+            "**/opencensus/proto/trace/v1/trace_config.proto",
+            "**/opencensus/proto/trace/v1/trace.proto",
+            "**/opencensus/proto/resource/v1/resource.proto",
+        ],
     );
+    collect_protos(
+        "opentelemetry-proto",
+        vec!["**/opentelemetry/proto/common/v1/common.proto"],
+    );
+    collect_protos("client_model", vec!["io/prometheus/client/metrics.proto"]);
 }
