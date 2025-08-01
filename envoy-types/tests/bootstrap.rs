12e87a9e1e080baacf35ede9834d50762c11184a
Rafael Lemos <contact@flemosr.com>
2025-08-01 12:01:14 -0300

    envoy-types: adjust `tests/bootstrap`
    Use `tonic-prost-build`, refactor.

--- envoy-types/tests/bootstrap.rs
+++ envoy-types/tests/bootstrap.rs
@@ -1,44 +1,46 @@
 use glob::glob;
 use std::{path::PathBuf, process::Command};
 
+const GLOB_PATTERNS: &[&str] = &[
+    "proto/data-plane-api/envoy/**/v3/*.proto",
+    // Add protos not included in the envoy proto's import tree (optional)
+    "proto/data-plane-api/envoy/type/http_status.proto",
+    "proto/googleapis/google/rpc/code.proto",
+];
+
+const INCLUDE_PATHS: &[&str] = &[
+    "proto/cel-spec/proto",
+    "proto/client_model",
+    "proto/data-plane-api",
+    "proto/googleapis",
+    "proto/opencensus-proto/src",
+    "proto/opentelemetry-proto",
+    "proto/opentelemetry-proto",
+    "proto/protoc-gen-validate",
+    "proto/xds",
+];
+
 #[test]
 fn bootstrap() {
     let out_dir = PathBuf::from("src/generated");
 
-    let glob_patterns = vec![
-        "proto/data-plane-api/envoy/**/v3/*.proto",
-        // Add protos not included in the envoy proto's import tree (optional)
-        "proto/data-plane-api/envoy/type/http_status.proto",
-        "proto/googleapis/google/rpc/code.proto",
-    ];
     let mut protos: Vec<PathBuf> = Vec::new();
 
-    for pattern in glob_patterns {
+    for pattern in GLOB_PATTERNS {
         let mut matches: Vec<PathBuf> = glob(pattern).unwrap().filter_map(Result::ok).collect();
         protos.append(&mut matches);
     }
 
-    tonic_build::configure()
+    let include_paths: Vec<PathBuf> = INCLUDE_PATHS.iter().map(PathBuf::from).collect();
+
+    tonic_prost_build::configure()
         .build_server(true)
         .build_client(true)
         .compile_well_known_types(true)
         .out_dir(&out_dir)
         .file_descriptor_set_path(out_dir.join("types.bin"))
         .include_file("mod.rs")
-        .compile_protos(
-            &protos,
-            &[
-                "proto/cel-spec/proto",
-                "proto/client_model",
-                "proto/data-plane-api",
-                "proto/googleapis",
-                "proto/opencensus-proto/src",
-                "proto/opentelemetry-proto",
-                "proto/opentelemetry-proto",
-                "proto/protoc-gen-validate",
-                "proto/xds",
-            ],
-        )
+        .compile_protos(&protos, &include_paths)
         .unwrap();
 
     let status = Command::new("git")
